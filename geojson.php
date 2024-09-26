bf59d0e46f38fbc385775f31fb1a0276120a3511
Fachrudin Effendi <52682756+masudinn@users.noreply.github.com>
2024-09-26 14:16:21 +0700

    Update geojson.php

--- geojson.php
+++ geojson.php
@@ -21,3 +21,45 @@ const map = L.map('map').setView([-7.761254962402551, 110.42463921766357], 16);
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         }).addTo(map);
+
+
+L.geoJson(gk, {
+            fillColor: 'Navy',
+            color: 'Navy',
+            fiilOpacity: 1,
+            weight: 1
+        }).bindPopup(
+            "Ini peta Gunung Kidul <br>" +
+            "<img src='../img/kab gk.jpg' width='200px'>").addTo(map);
+
+        L.geoJson(sleman, {
+            fillColor: 'Salmon',
+            color: 'Salmon',
+            fiilOpacity: 1,
+            weight: 1
+        }).bindPopup("Ini peta Sleman <br>" +
+            "<img src='../img/kab sleman.jpg' width='200px'> + ").addTo(map);
+
+        L.geoJson(bantul, {
+            fillColor: 'Amber',
+            color: 'Amber',
+            fiilOpacity: 1,
+            weight: 1
+        }).bindPopup("Ini peta Bantul <br>" +
+            "<img src='../img/kab bantul.jpg' width='200px'>").addTo(map);
+
+        L.geoJson(kp, {
+            fillColor: 'Orange',
+            color: 'Orange',
+            fiilOpacity: 1,
+            weight: 1
+        }).bindPopup("Ini peta Kulon Progo <br>" +
+            "<img src='../img/kab kulonprogo.jpg' width='200px'>").addTo(map);
+
+        L.geoJson(kota, {
+            fillColor: 'red',
+            color: 'red',
+            fiilOpacity: 1,
+            weight: 1
+        }).bindPopup("Ini peta Kota Yogyakarta <br>" +
+            "<img src='../img/kab kota.jpg' width='200px'>").addTo(map);
